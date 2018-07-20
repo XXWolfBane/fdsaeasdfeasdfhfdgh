90e903d689918cb5b3fe9c11f8936d2a625e33a3
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-20 12:14:00 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='$'
+bot = commands.Bot(command_prefix='.')
 
 @bot.event
 asycn def on_ready():
@@ -9,6 +9,11 @@ asycn def on_ready():
    print(bot.user.name)
    print(bot.userid)
    print('-------------')
+
+@bot.command()
+async def test(ctx):
+    await.ctx.send('This is a Testing command')
+      
  
 
 bot.run('NDY5ODkzMzI1NzgxMDczOTIz.DjOWvQ.l3CHq42SXWlAKyvJXuVD2FHmQjk')
