8ac1ef43387da8cef402c9cb78c48df459a36299
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-21 16:24:34 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,17 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='.')
+bot = commands.Bot(command_prefix='$')
 
 @bot.event
-asycn def on_ready():
-   print('Logged in as')
-   print(bot.user.name)
-   print(bot.userid)
-   print('-------------')
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 @bot.command()
-async def test(ctx):
-    await.ctx.send('This is a Testing command')
-      
- 
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
 
-bot.run('NDY5ODkzMzI1NzgxMDczOTIz.DjOWvQ.l3CHq42SXWlAKyvJXuVD2FHmQjk')
+bot.run('NDY5ODkzMzI1NzgxMDczOTIz.DjUn9Q.OcqLxPx9g6gEMSDzuTJSGJwuh4c')
